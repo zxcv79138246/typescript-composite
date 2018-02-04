a9c4128c669e30f30d0b666731b38d20e49a1adb
ChihChunLiu <zxcv79138246@gmail.com>
2018-02-05 01:58:36 +0800

    add composite

---
+++ src/composite.ts
@@ -0,0 +1,34 @@
+import { Shape } from "./shape";
+
+export class Composite extends Shape {
+  private list: Array<Shape>;
+  public constructor(public pointX = 0, public pointY = 0) {
+    super();
+    this.list = [];
+  }
+
+  public add(shape: Shape) {
+    this.list.push(shape);
+  }
+
+  public clear() {
+    this.list = [];
+  }
+
+  public draw(drawX = 0, drawY = 0) {
+    this.list.map(shape => {
+      let shapeDrawX = drawX + shape.pointX;
+      let shapeDrawY = drawY + shape.pointY;
+      shape.draw(shapeDrawX, shapeDrawY);
+    });
+  }
+
+  public area() {
+    let totalArea: number = 0;
+    this.list.map(shape => {
+      totalArea = totalArea + shape.area();
+    });
+
+    return totalArea;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "../style/index.scss";
 import { Shape } from "./shape";
 import { Rectangle } from "./rectangle";
 import { Triangle } from "./triangle";
+import { Composite } from "./composite";
 
 let canvas = document.getElementById("drawer1") as HTMLCanvasElement;
 canvas.width = 800;
@@ -15,3 +16,20 @@ console.log("rect1 area", rect1.area());
 let tri1 = new Triangle(100, 40, 100, "#00FF00", ctx);
 tri1.draw();
 console.log("tri1 area", tri1.area());
+
+let house = new Composite();
+house.add(rect1);
+house.add(tri1);
+house.draw(300, 200);
+
+console.log("houseArea", house.area());
+
+let rect2 = new Rectangle(200, 200, 50, 50, "#0000FF", ctx);
+rect2.draw();
+console.log("rect2 area", rect2.area());
+
+let composite2 = new Composite();
+composite2.add(house);
+composite2.add(rect2);
+composite2.draw(500, 10);
+console.log("composite2 area", composite2.area());
